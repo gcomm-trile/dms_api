a3f990fc05ec5da6abdb1fe434c7dfd431586866
gcomm-trile <58388336+gcomm-trile@users.noreply.github.com>
2020-10-11 21:17:26 +0700

    v
    v

---
+++ Controllers/DashboardTongHopController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess;
+using dms_api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace albus_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DashboardTongHopController : ControllerBase
+    {
+        private readonly ILogger<DashboardTongHopController> _logger;
+
+        public DashboardTongHopController(ILogger<DashboardTongHopController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet()]
+        public async Task<ActionResult<List<Report_TongHop>>> GetItem(DateTime from_date,DateTime to_date)
+        {
+            string sessionID
+              = Request.Headers["Session-ID"];
+            ClientServices Services = new ClientServices(sessionID);
+            var query = DataAccess.DataQuery.Create("dms", "rp_report_tonghop", new
+            {
+                from_date = from_date.ToString("yyyy-MM-dd HH:mm:ss"),
+                to_date = to_date.ToString("yyyy-MM-dd HH:mm:ss")
+            });
+
+            var ds = await Services.ExecuteAsync(query);
+            if (ds == null)
+            {
+                return BadRequest(Services.LastError);
+            }
+            else
+            {
+              
+                return ds.Tables[0].ToModel<Report_TongHop>();
+     
+               
+            }
+        }
+      
+
+    }
+}
---
+++ Models/Report.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dms_api.Models
+{
+    public class Report
+    {
+        public List<Report_TongHop> tonghop { get; set; }
+        public List<Report_Tuyen> tuyen { get; set; }
+        public List<Report_NVBH> nvbh { get; set; }
+
+    }
+    public class Report_TongHop
+    {
+        public string province { get; set; }
+        public int count_visit { get; set; }
+        public int count_store_order { get; set; }
+        public int count_order { get; set; }
+        public Int64 sum_order_price { get; set; }
+    }
+    public class Report_Tuyen
+    {
+        public Guid route_id { get; set; }
+        public string route_name { get; set; }
+        public string province { get; set; }
+        public int count_visit { get; set; }
+        public int count_store_order { get; set; }
+        public int count_order { get; set; }
+        public Int64 sum_order_price { get; set; }
+    }
+    public class Report_NVBH
+    {
+        public Guid user_id { get; set; }
+        public Guid route_id { get; set; }
+        public DateTime report_date { get; set; }
+        public string full_name { get; set; }
+        public string route { get; set; }
+        public string province { get; set; }
+        public int count_visit { get; set; }
+        public int count_store_order { get; set; }
+        public int count_order { get; set; }
+        public Int64 sum_order_price { get; set; }
+    }
+}
